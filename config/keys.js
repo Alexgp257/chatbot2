8b2beaf41b93206639bcd30ded809d35aeb4811e
Alex Pasca <alex.g.pasca@gmail.com>
2020-06-15 18:28:55 +0100

    console.log added

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
+    console.log("production");
     module.exports = require('./prod');
 } else {
+    console.log("DEV");
     module.exports = require('./dev');
 }
